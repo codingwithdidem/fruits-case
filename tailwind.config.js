af87f8bc10090bcb2a7407ecead9fd84ec4514ca
codingwithdidem <codingwithdidem@gmail.com>
2022-03-15 20:40:59 +0300

    added font family & color theme & background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fira: ["Fira Code", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "brand-black": "var(--brand-black)",
+        "brand-white": "var(--brand-white)",
+      },
+    },
   },
   plugins: [],
 };
